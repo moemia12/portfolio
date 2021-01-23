387d0f42bb5e2d66815a5bac91f45324541ddd69
Mohsin Miah <moemia12@gmail.com>
2021-01-23 14:09:17 +0000

    Final JS Update

--- orbitalmain.js
+++ orbitalmain.js
@@ -1,5 +1,5 @@
 // Declaring Earth, Icon and Answer Container variables
-               
+
 let icons = [...document.querySelectorAll('.icon-container .fa')];
 let answerContainer = document.getElementById("answer-container");
 let answerDiv = document.getElementById("answer-div");
@@ -7,43 +7,48 @@ let planetImgs = [...document.querySelectorAll(".planet-container img")];
 
 
 //Function to slide Planet
-function adjustActivePlanet (adjustment) { 
-  var current = planetImgs.find(it => it.id === 'active-planet'); 
+function adjustActivePlanet(adjustment) {
+  var current = planetImgs.find(it => it.id === 'active-planet');
   var currentIndex = planetImgs.indexOf(current);
   var nextIndex = (currentIndex + adjustment) % icons.length;
 
   if (nextIndex < 0) nextIndex = planetImgs.length - 1;
   current.removeAttribute('id');
-  planetImgs[nextIndex].id = 'active-planet'; 
+  planetImgs[nextIndex].id = 'active-planet';
 
+  adjustOrbitalPeriod();
 
-  }
+}
 
-    //Function to slide icons
-     function adjustActive (adjustment) { 
-     var current = icons.find(it => it.id === 'active'); 
-     var currentIndex = icons.indexOf(current);
-     var nextIndex = (currentIndex + adjustment) % icons.length;
+//Function to slide icons
+function adjustActive(adjustment) {
+  var current = icons.find(it => it.id === 'active');
+  var currentIndex = icons.indexOf(current);
+  var nextIndex = (currentIndex + adjustment) % icons.length;
 
-     if (nextIndex < 0) nextIndex = icons.length - 1;
-    current.removeAttribute('id');
-    icons[nextIndex].id = 'active'; 
+  if (nextIndex < 0) nextIndex = icons.length - 1;
+  current.removeAttribute('id');
+  icons[nextIndex].id = 'active';
 
-    // Action to find Orbital Period
-    let currentPlanet = document.getElementById("active-planet");
-    let planetRadius = currentPlanet.dataset.radius;
-    console.log(planetRadius)
-    let nextElement = document.getElementById("active");
-    let vehicleSpeed = nextElement.getAttribute("value")
-    let orbitalPeriod = planetRadius / vehicleSpeed;
+  adjustOrbitalPeriod();
 
-    answerDiv.innerHTML = orbitalPeriod.toFixed(2);
+}
 
-     }
+function adjustOrbitalPeriod() {
+  // Action to find Orbital Period
+  let currentPlanet = document.getElementById("active-planet");
+  let planetRadius = currentPlanet.dataset.radius;
+  let nextElement = document.getElementById("active");
+  let vehicleSpeed = nextElement.getAttribute("value")
+  let orbitalPeriod = planetRadius / vehicleSpeed;
+
+  answerDiv.innerHTML = orbitalPeriod.toFixed(2);
+}
 
 // Arrow Buttons
 
-document.querySelector('#left-arrow').addEventListener('click', e => adjustActive(-1), adjustActivePlanet(-1) );
-document.querySelector('#right-arrow').addEventListener('click', e => adjustActive(1), adjustActivePlanet(1));
-document.querySelector('#left-arrow-planet').addEventListener('click', e => adjustActivePlanet(-1),adjustActive(-1) );
-document.querySelector('#right-arrow-planet').addEventListener('click', e => adjustActivePlanet(1), adjustActive(1));
+document.querySelector('#left-arrow').addEventListener('click', e => adjustActive(-1));
+document.querySelector('#right-arrow').addEventListener('click', e => adjustActive(1));
+document.querySelector('#left-arrow-planet').addEventListener('click', e => adjustActivePlanet(-1));
+document.querySelector('#right-arrow-planet').addEventListener('click', e => adjustActivePlanet(1));
+
